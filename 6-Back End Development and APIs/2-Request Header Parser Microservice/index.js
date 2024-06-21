9fc1720a026067646dd6c50548a9f7f3d50559f9
“Kuwado” <viethoan557723@gmail.com>
2024-06-21 23:00:28 +0700

    6.3

--- 6-Back End Development and APIs/2-Request Header Parser Microservice/index.js
+++ 6-Back End Development and APIs/2-Request Header Parser Microservice/index.js
@@ -24,7 +24,22 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// API endpoint to return IP address, language, and software
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+module.exports = app;
